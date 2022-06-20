5a1ebad3402d16efbff41726cb4415b044228633
Manjunath Jidagi <manjujidagi@protonmail.com>
2022-06-20 15:18:07 +0530

    Updated Codes

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express()
 const axios = require('axios');
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 // if route starts with /item_types, redirect to http://localhost:3001
 app.all('/item_types', async (req, res) => {
     let result;
     try {
-        result = await axios({
+        let axios_obj = {
             method: req.method,
             url: 'http://localhost:3001' + req.originalUrl,
-            headers: {
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
                 'Authorization': req.headers.authorization
             }
-        });
+        }
+        result = await axios(axios_obj);
     } catch(e) {
         return res.status(e.response.status).send(e.response.data)
     }
@@ -21,13 +30,17 @@ app.all('/item_types', async (req, res) => {
 app.all('/item_types/*', async (req, res) => {
     let result;
     try {
-        result = await axios({
+        let axios_obj = {
             method: req.method,
             url: 'http://localhost:3001' + req.originalUrl,
-            headers: {
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
                 'Authorization': req.headers.authorization
             }
-        });
+        }
+        result = await axios(axios_obj);
     } catch(e) {
         return res.status(e.response.status).send(e.response.data)
     }
@@ -38,13 +51,17 @@ app.all('/item_types/*', async (req, res) => {
 app.all('/countries', async (req, res) => {
     let result;
     try {
-        result = await axios({
+        let axios_obj = {
             method: req.method,
             url: 'http://localhost:3001' + req.originalUrl,
-            headers: {
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
                 'Authorization': req.headers.authorization
             }
-        });
+        }
+        result = await axios(axios_obj);
     } catch(e) {
         return res.status(e.response.status).send(e.response.data)
     }
@@ -53,13 +70,17 @@ app.all('/countries', async (req, res) => {
 app.all('/countries/*', async (req, res) => {
     let result;
     try {
-        result = await axios({
+        let axios_obj = {
             method: req.method,
             url: 'http://localhost:3001' + req.originalUrl,
-            headers: {
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
                 'Authorization': req.headers.authorization
             }
-        });
+        }
+        result = await axios(axios_obj);
     } catch(e) {
         return res.status(e.response.status).send(e.response.data)
     }
@@ -70,13 +91,17 @@ app.all('/countries/*', async (req, res) => {
 app.all('/organizations', async (req, res) => {
     let result;
     try {
-        result = await axios({
+        let axios_obj = {
             method: req.method,
             url: 'http://localhost:3001' + req.originalUrl,
-            headers: {
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
                 'Authorization': req.headers.authorization
             }
-        });
+        }
+        result = await axios(axios_obj);
     } catch(e) {
         return res.status(e.response.status).send(e.response.data)
     }
@@ -85,13 +110,17 @@ app.all('/organizations', async (req, res) => {
 app.all('/organizations/*', async (req, res) => {
     let result;
     try {
-        result = await axios({
+        let axios_obj = {
             method: req.method,
             url: 'http://localhost:3001' + req.originalUrl,
-            headers: {
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
                 'Authorization': req.headers.authorization
             }
-        });
+        }
+        result = await axios(axios_obj);
     } catch(e) {
         return res.status(e.response.status).send(e.response.data)
     }
@@ -101,8 +130,25 @@ app.all('/organizations/*', async (req, res) => {
 
 
 // if route starts with /auth, redirect to http://localhost:3000
-app.all('/auth/*', (req, res) => {
-    res.redirect('http://localhost:3000' + req.originalUrl);
+app.all('/auth/*', async (req, res) => {
+    // res.redirect('http://localhost:3000' + req.originalUrl);
+    let result;
+    try {
+        let axios_obj = {
+            method: req.method,
+            url: 'http://localhost:3000' + req.originalUrl,
+            data: req.body
+        }
+        if(req?.headers?.authorization) {
+            axios_obj.headers = {
+                'Authorization': req.headers.authorization
+            }
+        }
+        result = await axios(axios_obj);
+    } catch(e) {
+        return res.status(e.response.status).send(e.response.data)
+    }
+    return res.status(result.status).send(result.data)
 });
 
 app.listen(5000, () => {
